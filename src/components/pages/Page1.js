2ca4594e156510e5434cf578bd7854413a703518
TommiS <tommi.supi@gmail.com>
2020-03-09 17:01:12 +0200

    pic

--- src/components/pages/Page1.js
+++ src/components/pages/Page1.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
+import pic1 from "../../pictures/gaming.jpg";
 
 class Page1 extends Component {
     render() {
         return (
             <div>
+                <img src={pic1} className="gaming-pic" alt="pic1" height={400}/>
+                <br/>
+
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid animi assumenda dignissimos esse hic
                 id ipsum labore molestias numquam odit placeat quidem reiciendis repellendus saepe similique tempora
                 tenetur totam, voluptatem!
